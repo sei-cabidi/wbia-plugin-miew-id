51c6ff9c129c967b8f36d197ac16d8676a1f0126
LashaO <otarashvililasha@gmail.com>
2023-06-18 14:05:10 +0200

    adds optuna sweep script

---
+++ wbia_miew_id/sweep.py
@@ -0,0 +1,42 @@
+import optuna
+import yaml
+from train import run  # Replace with your actual function
+
+def objective(trial):
+    # Load the YAML config file
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+        
+    # Specify the parameters you want to optimize
+    config.data.train_n_filter_min = trial.suggest_int('train_n_filter_min', 2, 5)
+    image_size = trial.suggest_categorical('image_size', [192, 256, 384, 440, 512])
+    config.data.image_size = [image_size, image_size]
+    # config.data.epochs = trial.suggest_int('epochs', 20, 50)
+    config.model_params.margin = trial.suggest_uniform('margin', 0.1, 0.7)
+    config.model_params.s = trial.suggest_uniform('s', 20, 64)
+
+
+    # The scheduler params are derived from one base paremeter to minimize the number of parameters to optimzie
+    lr_base = trial.suggest_loguniform('lr_base', 1e-6, 1e-2)
+    config.scheduler_params.lr_start = lr_base
+    config.scheduler_params.lr_max = lr_base * 10
+    config.scheduler_params.lr_min = lr_base / 2
+
+    config.scheduler_params
+
+    result = run(config)
+    
+    return result
+
+# Create a study object and optimize the objective
+study = optuna.create_study(direction='maximize')  # or 'minimize' depending on your metric
+study.optimize(objective, n_trials=100)
+
+print('Best trial:')
+trial_ = study.best_trial
+
+print(f'Value: {trial_.value}')
+
+print('Best parameters:')
+for key, value in trial_.params.items():
+    print(f'    {key}: {value}')
