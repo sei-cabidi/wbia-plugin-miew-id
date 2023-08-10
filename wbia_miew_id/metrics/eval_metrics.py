282226038d0bb6b22b80a5256d071f24dd42afb7
LashaO <otarashvililasha@gmail.com>
2023-08-10 17:11:02 +0400

    Adds vectorized map calculation code(not used yet)

--- wbia_miew_id/metrics/eval_metrics.py
+++ wbia_miew_id/metrics/eval_metrics.py
@@ -2,7 +2,7 @@ import torch
 
 def precision_at_k(names, distmat, k=5, return_matches=False):
     """Computes precision at k given a distance matrix. 
-    Assumes the distance matrix is square for one-vs-all evaluation"""
+    Assumes the distance matrix is square and does one-vs-all evaluation"""
     assert distmat.shape[0] == distmat.shape[1], "Distance matrix must be square"
 
     output = torch.Tensor(distmat[:, :]) * -1
@@ -21,3 +21,28 @@ def precision_at_k(names, distmat, k=5, return_matches=False):
         return score, match_mat, topk_idx, topk_names
     else:
         return score
+    
+
+def topk_average_precision(names, distmat, k=None):
+    """Computes top-k average precision given a distance matrix.
+    Assumes the distance matrix is square and does one-vs-all evaluation"""
+    assert distmat.shape[0] == distmat.shape[1], "Distance matrix must be square"
+
+    output = torch.Tensor(distmat[:, :]) * -1
+    y = torch.Tensor(names[:]).squeeze(0)
+    ids_tensor = torch.Tensor(names)
+
+    if k==None:
+        k = output.shape[1] - 1
+    score_array = torch.tensor([1.0 / i for i in range(1, k + 1)], device=output.device)
+
+    _topk = output.topk(k+1)[1][:, 1:]
+    topk = ids_tensor[_topk]
+
+    match_mat = topk == y[:, None].expand(topk.shape)
+    rel_mat = match_mat.sum(axis=1)
+    cum_mat = match_mat.cumsum(dim=1)
+
+    ap_mat = ((cum_mat * score_array) * match_mat).sum(axis=1) / rel_mat
+
+    return ap_mat.nan_to_num(0).mean()
