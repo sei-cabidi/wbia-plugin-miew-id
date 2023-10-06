d421c5c82095a33fa6660714432b5ce4df67f80c
LashaO <otarashvililasha@gmail.com>
2023-10-06 16:56:55 +0400

    Update sweep.py

--- wbia_miew_id/sweep.py
+++ wbia_miew_id/sweep.py
@@ -5,6 +5,7 @@ from helpers import get_config
 from optuna.pruners import MedianPruner
 from optuna.samplers import TPESampler
 import pickle
+import signal, sys
 
 
 import argparse
@@ -24,17 +25,17 @@ def parse_args():
 def objective(trial, config):
 
     # Specify the parameters you want to optimize
-    config.data.train.n_filter_min = trial.suggest_int("train.n_filter_min", 2, 5)
-    image_size = trial.suggest_categorical("image_size", [192, 256, 384, 440, 512])
-    config.data.image_size = [image_size, image_size]
-    n_epochs = trial.suggest_int("epochs", 20, 40)
+    config.data.train.n_filter_min = 4 #trial.suggest_int("train.n_filter_min", 2, 5)
+    # image_size = 256 #trial.suggest_categorical("image_size", [192, 256, 384, 440, 512])
+    # config.data.image_size = [image_size, image_size]
+    n_epochs = 20#trial.suggest_int("epochs", 20, 40)
     config.engine.epochs = n_epochs
-    config.model_params.margin = trial.suggest_uniform("margin", 0.1, 0.7)
+    config.model_params.margin = trial.suggest_uniform("margin", 0.3, 0.7)
     config.model_params.s = trial.suggest_uniform("s", 20, 64)
 
     # The scheduler params are derived from one base paremeter to minimize the number of parameters to optimzie
     lr_base = trial.suggest_loguniform("lr_base", 1e-6, 1e-2)
-    config.scheduler_params.lr_start = lr_base
+    config.scheduler_params.lr_start = lr_base / 10
     config.scheduler_params.lr_max = lr_base * 10
     config.scheduler_params.lr_min = lr_base / 20
 
@@ -51,6 +52,16 @@ def objective(trial, config):
 
     return result
 
+## Probably only needed to reproduce the results of same seed
+def signal_handler(signum, frame):
+    global study_sampler
+    print("\nSaving the current state of the study before exiting...")
+    with open('study_sampler.pkl', 'wb') as f:
+        pickle.dump(study, f)
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 
 if __name__ == "__main__":
     args = parse_args()
@@ -58,8 +69,13 @@ if __name__ == "__main__":
 
     config = get_config(config_path)
 
+    study_name = config.exp_name
+    study_storage = f"sqlite:///{study_name}.db"
+    study_sampler = TPESampler(seed=0)
+
     study = optuna.create_study(
-        sampler=TPESampler(), pruner=MedianPruner(), direction="maximize"
+        study_name=study_name, storage=study_storage,
+        sampler=study_sampler, pruner=MedianPruner(), direction="maximize", load_if_exists=True
     )
 
     comb_objective = lambda trial: objective(trial, config)
