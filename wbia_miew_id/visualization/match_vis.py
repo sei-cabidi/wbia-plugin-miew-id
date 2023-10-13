253c9d097bf33147e4691a3d85582f399cff72e1
LashaO <otarashvililasha@gmail.com>
2023-10-13 14:33:17 +0400

    fix match vis index display

--- wbia_miew_id/visualization/match_vis.py
+++ wbia_miew_id/visualization/match_vis.py
@@ -31,11 +31,12 @@ def stack_match_images(images, descriptions, match_mask, text_color=(0, 0, 0)): 
         bw = 12
         img = cv2.copyMakeBorder(img, bw, bw, bw, bw, cv2.BORDER_CONSTANT, value=color)
 
-        (tw, th), _ = cv2.getTextSize(desc_qry, cv2.FONT_HERSHEY_SIMPLEX, 2, 4)
+        font_scale = 1
+        (tw, th), _ = cv2.getTextSize(desc_qry, cv2.FONT_HERSHEY_SIMPLEX, font_scale, 4)
         text_img = np.ones((int(th * 2), img.shape[1], 3), dtype=np.uint8) * 255  # Change height as needed
         
-        cv2.putText(text_img, desc_qry, (th, int(th * 1.5)), cv2.FONT_HERSHEY_SIMPLEX, 2, text_color, 4)
-        cv2.putText(text_img, desc_db, (img.shape[1]//2 + th, int(th * 1.5)), cv2.FONT_HERSHEY_SIMPLEX, 2, text_color, 4)
+        cv2.putText(text_img, desc_qry, (th, int(th * 1.5)), cv2.FONT_HERSHEY_SIMPLEX, font_scale, text_color, 4)
+        cv2.putText(text_img, desc_db, (img.shape[1]//2 + th, int(th * 1.5)), cv2.FONT_HERSHEY_SIMPLEX, font_scale, text_color, 4)
         result_images.extend([text_img, img])
 
     result = np.vstack(result_images)
@@ -56,15 +57,16 @@ def render_single_query_result(config, model, vis_loader, df_vis, qry_row, qry_i
     indices = df_vis.index.values
     qry_name = qry_row['name']
     qry_viewpoint = qry_row['viewpoint']
+    qry_loc_idx = qry_row.name
 
-    desc_qry = [f"Query: {qry_name} {qry_viewpoint} ({qry_idx})" for i in range(len(viewpoints))]
+    desc_qry = [f"Query: {qry_name} {qry_viewpoint} ({qry_loc_idx})" for i in range(len(viewpoints))]
     desc_db = [f"Match: {name} {viewpoint} ({idx})" for name, viewpoint, idx in zip(names, viewpoints, indices)]
     descriptions = [(q, d) for q, d in zip(desc_qry, desc_db)]
 
     vis_result = stack_match_images(batch_images, descriptions, vis_match_mask)
 
     output_dir = f"{config.checkpoint_dir}/{config.project_name}/{config.exp_name}/visualizations"
-    output_name = f"vis_{qry_name}_{qry_viewpoint}_{qry_idx}_top{k}.jpg"
+    output_name = f"vis_{qry_name}_{qry_viewpoint}_{qry_loc_idx}_top{k}.jpg"
     output_path = os.path.join(output_dir, output_name)
 
     os.makedirs(output_dir, exist_ok=True)
@@ -94,7 +96,7 @@ def render_query_results(config, model, test_dataset, df_test, match_results, k=
         vis_loader = torch.utils.data.DataLoader(
                 test_dataset,
                 batch_size=config.engine.valid_batch_size,
-                num_workers=config.engine.num_workers,
+                num_workers=0,
                 shuffle=False,
                 pin_memory=True,
                 drop_last=False,
