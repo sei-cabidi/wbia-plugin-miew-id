fa337c55e0382691a7140b0b83bac4405d1ac9c5
LashaO <otarashvililasha@gmail.com>
2023-10-13 14:32:52 +0400

    Refactor preprocess functions

--- wbia_miew_id/etl/preprocess.py
+++ wbia_miew_id/etl/preprocess.py
@@ -26,6 +26,8 @@ def load_to_df(anno_path):
     else:
         df = dfa.merge(dfi, left_on='image_id', right_on='id') 
 
+    print(f'** Loaded {anno_path} **')
+    print('     ', f'Found {len(df)} annotations')
 
     return df
 
@@ -39,9 +41,9 @@ def filter_min_names_df(df, n_filter_min, filter_key='name_species'):
     print('     ', len(df), 'annotations remain after filtering by min', n_filter_min, 'per', filter_key)
     return df
 
-def subsample_max_df(df, n_subsample_max, subsample_name_key='name_species'):
-    df = df.groupby(subsample_name_key).apply(lambda g: g.sample(frac=1, random_state=0).head(n_subsample_max)).sample(frac=1, random_state=1).reset_index(drop=True)
-    print('     ', len(df), 'annotations remain after subsampling by max', n_subsample_max, 'per', subsample_name_key)
+def subsample_max_df(df, n_subsample_max, filter_key='name_species'):
+    df = df.groupby(filter_key, as_index=False).apply(lambda g: g.sample(frac=1, random_state=0).head(n_subsample_max)).droplevel(level=0)
+    print('     ', len(df), 'annotations remain after subsampling by max', n_subsample_max, 'per', filter_key)
     return df
 
 def convert_name_to_id(names):
@@ -49,12 +51,25 @@ def convert_name_to_id(names):
     names_id = le.fit_transform(names)
     return names_id
 
+def filter_df(df, viewpoint_list, n_filter_min, n_subsample_max, filter_key='name_species'):
+    if viewpoint_list:
+        df = filter_viewpoint_df(df, viewpoint_list)
+    
+    if n_filter_min:
+        df = filter_min_names_df(df, n_filter_min, filter_key=filter_key)
+
+    if not len(df):
+        raise Exception("No samples remain after filtering.")
+        
+    if n_subsample_max:
+        df = subsample_max_df(df, n_subsample_max, filter_key=filter_key)
+
+    return df
+
 def preprocess_data(anno_path, name_keys=['name'], convert_names_to_ids=True, viewpoint_list=None, n_filter_min=None, n_subsample_max=None, use_full_image_path=False, images_dir=None):
 
     df = load_to_df(anno_path)
 
-    print(f'** Loaded {anno_path} **')
-    print('     ', f'Found {len(df)} annotations')
 
     df['name'] = df[name_keys].apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
 
@@ -63,16 +78,7 @@ def preprocess_data(anno_path, name_keys=['name'], convert_names_to_ids=True, vi
 
     df['name_orig'] = df['name'].copy()
 
-    if viewpoint_list:
-        df = filter_viewpoint_df(df, viewpoint_list)
-        
-    
-    if n_filter_min:
-        df = filter_min_names_df(df, n_filter_min)
-        
-
-    if n_subsample_max:
-        df = subsample_max_df(df, n_subsample_max)
+    df = filter_df(df, viewpoint_list, n_filter_min, n_subsample_max)
 
     if convert_names_to_ids:
         names = df['name'].values
