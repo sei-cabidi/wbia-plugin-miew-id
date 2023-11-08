7f48ed7857b12bf26917afbc83ee91ba1ddb481c
Collin Abidi <cabidi@sei.cmu.edu>
2023-11-08 13:50:40 -0500

    added swa changes

--- wbia_miew_id/engine/run_fn.py
+++ wbia_miew_id/engine/run_fn.py
@@ -6,21 +6,92 @@ import numpy as np
 from .train_fn import train_fn
 from .eval_fn import eval_fn
 
+@torch.no_grad()
+def update_bn(loader, model, device=None):
+    r"""Updates BatchNorm running_mean, running_var buffers in the model.
 
-def run_fn(config, model, train_loader, valid_loader, criterion, optimizer, scheduler, device, checkpoint_dir, use_wandb=True):
+    This is a copy of torch.optim.swa_utils.update_bn that allows us to
+    pass in a DataLoader that has batches of dictionaries with the image
+    stored in a field called 'input'
+
+    It performs one pass over data in `loader` to estimate the activation
+    statistics for BatchNorm layers in the model.
+    Args:
+        loader (torch.utils.data.DataLoader): dataset loader to compute the
+            activation statistics on. Each data batch should be either a
+            tensor, or a list/tuple whose first element is a tensor
+            containing data.
+        model (torch.nn.Module): model for which we seek to update BatchNorm
+            statistics.
+        device (torch.device, optional): If set, data will be transferred to
+            :attr:`device` before being passed into :attr:`model`.
+
+    Example:
+        >>> # xdoctest: +SKIP("Undefined variables")
+        >>> loader, model = ...
+        >>> torch.optim.swa_utils.update_bn(loader, model)
+
+    .. note::
+        The `update_bn` utility assumes that each data batch in :attr:`loader`
+        is either a tensor or a list or tuple of tensors; in the latter case it
+        is assumed that :meth:`model.forward()` should be called on the first
+        element of the list or tuple corresponding to the data batch.
+    """
+    momenta = {}
+    for module in model.modules():
+        if isinstance(module, torch.nn.modules.batchnorm._BatchNorm):
+            module.running_mean = torch.zeros_like(module.running_mean)
+            module.running_var = torch.ones_like(module.running_var)
+            momenta[module] = module.momentum
+
+    if not momenta:
+        return
+
+    was_training = model.training
+    model.train()
+    for module in momenta.keys():
+        module.momentum = None
+        module.num_batches_tracked *= 0
+
+    for input in loader:
+        if isinstance(input, (list, tuple)):
+            input = input[0]
+        elif isinstance(input, dict):
+            label = input['label']
+            input = input['image']
+        if device is not None:
+            input = input.to(device)
+            label = label.to(device)
+
+        model(input, label=label)
+
+    for bn_module in momenta.keys():
+        bn_module.momentum = momenta[bn_module]
+    model.train(was_training)
+
+def run_fn(config, model, train_loader, valid_loader, criterion, optimizer, scheduler, device, checkpoint_dir, use_wandb=True, swa_model=None, swa_start=None, swa_scheduler=None):
 
     best_score = 0
     for epoch in range(config.engine.epochs):
 
-        train_loss = train_fn(train_loader, model,criterion, optimizer, device,scheduler=scheduler,epoch=epoch, use_wandb=use_wandb)
+        train_loss = train_fn(train_loader, model,criterion, optimizer, device,scheduler=scheduler,epoch=epoch, use_wandb=use_wandb, swa_model=swa_model, swa_start=swa_start, swa_scheduler=swa_scheduler)
 
-        # torch.save(model.state_dict(), f'{checkpoint_dir}/model_{epoch}.bin')
+        torch.save(model.state_dict(), f'{checkpoint_dir}/model_{epoch}.bin')
         
-        valid_score, cmc = eval_fn(valid_loader, model, device, use_wandb=use_wandb)
-        
+        valid_score, cmc, output = eval_fn(valid_loader, model, device, use_wandb=use_wandb, return_outputs=True)
+        diagram = output[-1]
+        diagram.savefig(f"reliability_diagram_val_{epoch}.pdf")
+            
         if valid_score > best_score:
             best_score = valid_score
             torch.save(model.state_dict(), f'{checkpoint_dir}/model_best.bin')
             print('best model found for epoch {}'.format(epoch))
 
+    if swa_model:
+        # Update bn statistics for the swa_model at the end
+        print("Updating SWA batchnorm statistics...")
+        update_bn(train_loader, model, device=device)
+        torch.save(swa_model.state_dict(), f'{checkpoint_dir}/swa_model_{epoch}.bin')
+        #valid_score_swa, cmc_swa = eval_fn(valid_loader, swa_model, device, use_wandb=use_wandb)
+    
     return best_score
--- wbia_miew_id/engine/train_fn.py
+++ wbia_miew_id/engine/train_fn.py
@@ -1,43 +1,89 @@
+import torch
+import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
 import wandb
-from metrics import AverageMeter
+from metrics import AverageMeter, compute_calibration
+from visualization.reliability_diagrams.reliability_diagrams import reliability_diagram
 
 
-def train_fn(dataloader,model,criterion,optimizer,device,scheduler,epoch, use_wandb=True):
+def train_fn(dataloader, model, criterion, optimizer, device, scheduler, epoch, use_wandb=True, swa_model=None, swa_start=None, swa_scheduler=None):
     model.train()
     loss_score = AverageMeter()
     tk0 = tqdm(enumerate(dataloader), total=len(dataloader))
-    for bi,d in tk0:
+    all_targets = []
+    all_images = []
+    all_outputs = []
+    for bi, d in tk0:
         images = d['image']
         targets = d['label']
         batch_size = images.shape[0]
 
-
- 
         images = images.to(device)
         targets = targets.to(device)
 
         optimizer.zero_grad()
 
-        output = model(images,targets)
+        output = model(images, targets)
         
-        loss = criterion(output,targets)
-        
+        if epoch > 13:
+            import pdb
+            pdb.set_trace()
+            # Are targets here the same as targets in eval_fn?
+
+        loss = criterion(output, targets)
+
         loss.backward()
         optimizer.step()
-        
+
+        batch_accuracy = (targets == torch.softmax(
+            output, dim=1).argmax(dim=1)).sum() / targets.shape[0]
+
         loss_score.update(loss.detach().item(), batch_size)
-        tk0.set_postfix(Train_Loss=loss_score.avg,Epoch=epoch,LR=optimizer.param_groups[0]['lr'])
-        
-    if scheduler is not None:
-            scheduler.step()
+        tk0.set_postfix(Train_Loss=loss_score.avg, Epoch=epoch,
+                        LR=optimizer.param_groups[0]['lr'], TrainAcc=batch_accuracy.item())
+
+        all_outputs.append(output.detach().cpu())
+        all_targets.append(targets.detach().cpu())
+        all_images.append(images.detach().cpu())
+
+
+    all_outputs = torch.cat(all_outputs)
+    all_targets = torch.cat(all_targets).numpy()
+    all_images = torch.cat(all_images).numpy()
+    confidences = torch.softmax(all_outputs, dim=1)
+    pred_targets = confidences.argmax(dim=1).numpy()
+    confidences = confidences.numpy().max(axis=1)
+    
+    if epoch > 28:
+        import pdb
+        pdb.set_trace()
+
+    if swa_model and epoch > swa_start:
+        print("Updating swa model...")
+        swa_model.update_parameters(model)
+        swa_scheduler.step()
+    else:
+        scheduler.step()
+
+    # Calibration metrics
+    print("Computing ECE ...")
+    metrics = compute_calibration(
+        all_targets, pred_targets, confidences, num_bins=10)
+    ECE = metrics['expected_calibration_error']
+    print(f"Epoch {epoch} ECE: {ECE}")
+
+    print("Drawing reliability diagram...")
+    diagram = reliability_diagram(
+        all_targets, pred_targets,  confidences, num_bins=10, return_fig=True)
+    diagram.savefig(f"reliability_diagram_train_{epoch}.pdf")
+    plt.close()
 
     if use_wandb:
         wandb.log({
             "train loss": loss_score.avg,
             "epoch": epoch,
-            "lr": optimizer.param_groups[0]['lr']
-            })
-        
-    return loss_score
+            "lr": optimizer.param_groups[0]['lr'],
+            "batch_accuracy": batch_accuracy
+        })
 
+    return loss_score
