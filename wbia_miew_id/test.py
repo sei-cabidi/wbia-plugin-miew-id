7128839b37016e6226b8814fda10eda915606623
LashaO <otarashvililasha@gmail.com>
2023-11-20 18:49:47 +0400

    Fixes visualization texts

--- wbia_miew_id/test.py
+++ wbia_miew_id/test.py
@@ -104,7 +104,7 @@ def run_test(config, visualize=False):
     eval_groups = config.data.test.eval_groups
 
     if eval_groups:
-        df_test = preprocess_data(config.data.test.anno_path, 
+        df_test_group = preprocess_data(config.data.test.anno_path, 
                             name_keys=config.data.name_keys,
                             convert_names_to_ids=True, 
                             viewpoint_list=config.data.viewpoint_list, 
@@ -112,7 +112,7 @@ def run_test(config, visualize=False):
                             n_subsample_max=None,
                             use_full_image_path=config.data.use_full_image_path,
                             images_dir = config.data.images_dir)
-        group_results = group_eval(config, df_test, eval_groups, model)
+        group_results = group_eval(config, df_test_group, eval_groups, model)
 
     if visualize:
         k=5
