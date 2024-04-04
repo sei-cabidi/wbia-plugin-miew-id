1a31111e0acdfb449c2c9c12a1f60c13d622cd0d
LashaO <otarashvililasha@gmail.com>
2024-04-04 17:47:49 +0400

    Adds species field key to DataLoader

--- wbia_miew_id/datasets/default_dataset.py
+++ wbia_miew_id/datasets/default_dataset.py
@@ -24,31 +24,14 @@ class MiewIdDataset(Dataset):
         row = self.csv.iloc[index]
 
         image_path = row['file_path']
-
         image = load_image(image_path)
+
         bbox = row['bbox']
         theta = row['theta'] if row['theta'] is not None else 0
-
-        # if self.crop_bbox:
-        #     x1, y1, w, h = [int(x) for x in bbox]
-        #     image = image[y1 : y1 + h, x1 : x1 + w]
-        #     if min(image.shape) < 1:
-        #         # Use original image
-        #         print('Using original image. Invalid bbox', bbox)
-        #         print(image_path)
-        #         image = self.load_image(image_path)
-        #         bbox = [0, 0, image.shape[1], image.shape[0]]
+        species = row.get('species', 'none')
 
         if self.crop_bbox:
             image = get_chip_from_img(image, bbox, theta)
-
-        # species = row['species']
-        # exclude_flip = ['']
-        # if species not in exclude_flip:
-        #     if random.random() < 0.5:
-        #         image = cv2.flip(image, 1)
-        #         bbox[0] = image.shape[1] - bbox[0] - bbox[2]
-
 
         if self.augmentations:
             augmented = self.augmentations(image=image)
@@ -61,4 +44,4 @@ class MiewIdDataset(Dataset):
         
         return {"image": image, "label":torch.tensor(row['name']), 
                 "image_idx": self.csv.index[index], "file_path": image_path, "bbox": torch.Tensor(bbox),
-                'theta': theta}
+                'theta': theta, 'species': species}
